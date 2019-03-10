71af6176fd9106aa28c4c3b726926d6f23b61547
Taesu Hyeon <qvil1127@gmail.com>
2019-03-11 00:59:58 +0900

    Add figlet for ascii-art but not working
    - Is it because execute eval function in parcel buncler?

--- src/demo.js
+++ src/demo.js
@@ -1,5 +1,25 @@
 // const SignatureCode = require("./index");
 import SignatureCode from "./index";
+import figlet from "figlet";
 
+// figlet.defaults({ fontPath: window.location.origin + "/assets/ascii-fonts" });
+figlet.defaults({ fontPath: "/assets/ascii-fonts" });
+
+figlet.text(
+  "Boo!",
+  {
+    font: "Ghost",
+    horizontalLayout: "default",
+    verticalLayout: "default"
+  },
+  function(err, data) {
+    if (err) {
+      console.log("Something went wrong...");
+      console.dir(err);
+      return;
+    }
+    console.log(data);
+  }
+);
 SignatureCode.toHTML("My Signature!");
 SignatureCode.toConsole("My Signature!");
