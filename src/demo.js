80673059d78601f2a434ad2124d21aa4b59ec5c0
Taesu Hyeon <qvil1127@gmail.com>
2019-03-28 04:17:57 +0900

    Temporary disable "figlet" have some error

--- src/demo.js
+++ src/demo.js
@@ -1,25 +1,35 @@
-// const SignatureCode = require("./index");
 import SignatureCode from "./index";
-import figlet from "figlet";
+// import figlet from "figlet";
+
+// figlet.text(
+//   "Boo!",
+//   {
+//     font: "Malgun Gothic",
+//     horizontalLayout: "default",
+//     verticalLayout: "default"
+//   },
+//   function(err, data) {
+//     if (err) {
+//       console.log("Something went wrong...");
+//       console.dir(err);
+//       return;
+//     }
+//     console.log(data);
+//   }
+// );
+
+// figlet.defaults({ fontPath: "/assets/ascii-fonts" });
+
+// figlet("Hello World!!", function(err, data) {
+//   if (err) {
+//     console.log("Something went wrong...");
+//     console.dir(err);
+//     return;
+//   }
+//   console.log(data);
+// });
 
 // figlet.defaults({ fontPath: window.location.origin + "/assets/ascii-fonts" });
-figlet.defaults({ fontPath: "/assets/ascii-fonts" });
 
-figlet.text(
-  "Boo!",
-  {
-    font: "Ghost",
-    horizontalLayout: "default",
-    verticalLayout: "default"
-  },
-  function(err, data) {
-    if (err) {
-      console.log("Something went wrong...");
-      console.dir(err);
-      return;
-    }
-    console.log(data);
-  }
-);
 SignatureCode.toHTML("My Signature!");
 SignatureCode.toConsole("My Signature!");
